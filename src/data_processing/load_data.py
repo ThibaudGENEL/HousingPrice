1fec31f2b54c6db634354a6adf020d1f1099355c
ThibaudGNL <thibaud.genel10@gmail.com>
2023-09-18 10:10:48 +0200

    add func load data

--- src/data_processing/load_data.py
+++ src/data_processing/load_data.py
@@ -2,13 +2,17 @@ import pandas as pd
 
 
 def load_data(filename: str) -> pd.DataFrame:
-    """_summary_
+    """Load a CSV file and return its data as a DataFrame.
 
     Args:
-        filename (str): _description_
+        filename (str): The name of the CSV file to load.
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: A DataFrame containing the data from the CSV file.
+
+    Example:
+        To load a CSV file named 'data.csv,' you can use this function as follows:
+        
+        ">>> data = load_data('data.csv')
     """
     return pd.read_csv(filename)
-
