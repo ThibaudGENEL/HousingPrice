140323a25ae5f196cba000bf78b587e8d59ec64a
ThibaudGNL <thibaud.genel10@gmail.com>
2023-09-19 14:25:46 +0200

    amelioration dataviz_univariate with categ

--- src/figures/dataviz.py
+++ src/figures/dataviz.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("df/house_prices.csv")
+df = pd.read_csv("data/house_prices.csv")
 
 
 def dataviz_univariate(df):
@@ -16,9 +16,9 @@ def dataviz_univariate(df):
 
     # Parcourir chaque colonne
     for col in cols_to_analyze:
-        # On s'assure que la colonne est numérique
+        # Si colonne est numérique
         if pd.api.types.is_numeric_dtype(df[col]):
-            # Créer une figure avec deux sous-graphiques
+            # Créer une figure avec deux sous-graphiques : Histo + Boxplot
             fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
 
             # Histogramme
@@ -42,6 +42,29 @@ def dataviz_univariate(df):
 
             # Afficher les deux sous-graphiques côte à côte
             plt.show()
+        # Si categorielle    
+        elif pd.api.types.is_object_dtype(df[col]) or pd.api.types.is_bool_dtype(df[col]):
+
+            # Si le nombre de catégories est inférieur ou égal à 3, un cemembert
+            if df[col].nunique() <= 3:
+                plt.figure(figsize=(5, 3))
+                plt.pie(df[col].value_counts(normalize = True), autopct="%1.1f%%", labels = df[col].value_counts().index) 
+                plt.title(f'{col}', weight='bold')
+                plt.text(1, -0.1, f'n = {df[col].count()}', 
+                horizontalalignment='right', verticalalignment='top', fontsize=8, 
+                transform=plt.gca().transAxes)
+                plt.show()        
+            # Si le nombre de catégories est inférieur ou égal à 10, un diagramme à barres
+            elif df[col].nunique() <= 10:
+                plt.figure(figsize=(8, 4))
+                sns.countplot(x=df[col])
+                plt.title(f'{col} Occurences', weight='bold', fontname='Times New Roman')
+                plt.xlabel(f'{col}', weight='bold', fontname='Times New Roman')
+                plt.xticks(rotation=30)
+                plt.text(1, -0.15, f'n = {df[col].count()}', 
+                horizontalalignment='right', verticalalignment='top', fontsize=8, 
+                transform=plt.gca().transAxes)
+                plt.show()    
 
 # Utilisation de la fonction dataviz avec un DataFrame exemple
 # dataviz(df)
