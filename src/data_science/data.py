3be0320f8422577d72d861ff01ec78edc4d34c8d
gaffiero karim <gaffierokarim@gmail.com>
2023-10-16 14:57:15 +0200

    adding split_data function and encoding the dataset

--- src/data_science/data.py
+++ src/data_science/data.py
@@ -5,15 +5,37 @@ import numpy as np
 def split_data(df: pd.DataFrame,
                test_ratio: float,
                seed: int) -> tuple[pd.DataFrame]:
-    """Diviser le dataset en set de train et test en mélangeant l'ordre des
-    données aléatoirement en fixant la random seed.
+    """Divide the dataset into train and test sets by mixing the order of
+    data randomly by setting the random seed.
 
     Args:
-        df (pd.DataFrame): _description_
-        test_ratio (float): _description_
-        seed (int): _description_
+        df (pd.DataFrame): Labeled dataframe
+        test_ratio (float): The ratio between 0 and 1 for the proportion of examples in the test set.
+        seed (int): The seed value for randomization.
 
     Returns:
         tuple[pd.DataFrame]: X_train, y_train, X_test, y_test
     """
-    pass
+
+
+
+    len_df = len(df)
+
+    np.random.seed(seed)
+    
+    index = np.random.permutation(df.index)
+    split_value= int(len_df * (1-test_ratio))
+
+    train_df, test_df = df.loc[index[:split_value]], df.loc[index[split_value:]]
+
+    X_train = train_df.drop('Price', axis=1)
+    y_train = train_df['Price']
+    X_test = test_df.drop('Price', axis=1)
+    y_test = test_df['Price']
+
+    return X_train, y_train, X_test, y_test
+
+
+
+
+
